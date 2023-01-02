ff9765dbdbdd75d0d4e1347127f6495067900a29
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2023-01-02 22:11:49 +0000

    JS file ( IF condition )

---
+++ JavaScript/Class01/ex08.js
@@ -0,0 +1,9 @@
+var vel = 99
+
+console.log(`The velocity of the car is ${vel} km/h`)
+
+if (vel > 60) {
+    console.log(`The velocity of the car is over the maximum limit!`)
+}
+
+console.log(`Drive safely`)
---
+++ JavaScript/Class01/ex09.js
@@ -0,0 +1,9 @@
+var país = 'United States'
+
+console.log(`You are from ${país}`)
+
+if (país == 'Portugal') {
+    console.log(`Talking Portuguese of Portugal`) }
+else {
+    console.log(`Talking American`)
+}
