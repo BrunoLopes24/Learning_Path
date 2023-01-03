4677db570440290a9496b828a04798ee53f56518
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2023-01-03 12:53:44 +0000

    Age using  (IF, ELSEIF)

---
+++ JavaScript/Class01/ex12.js
@@ -0,0 +1,9 @@
+var age = 22
+
+if (age < 16) {
+    console.log("You CANNOT vote!") 
+} else if (age <18 || age > 67) {
+    console.log("You can choose to vote")
+} else {
+    console.log("You MUST vote")
+}
