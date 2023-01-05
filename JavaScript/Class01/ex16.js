85469df10507ae8c87571558adc36a6dd4538195
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2023-01-05 12:14:05 +0000

    while structure

---
+++ JavaScript/Class01/ex16.js
@@ -0,0 +1,20 @@
+var count = 1
+
+while (count <= 6) {
+    console.log(`${count} x`)
+    count++ //  count = count + 1 
+}
+
+
+
+
+
+/* 
+OLD USE:
+console.log("Everything is Ok")
+console.log("Everything is Ok")
+console.log("Everything is Ok")
+console.log("Everything is Ok")
+console.log("Everything is Ok")
+console.log("Everything is Ok")
+*/
