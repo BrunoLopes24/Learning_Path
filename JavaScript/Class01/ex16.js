5253b973096c0d2589e44e138678e1f0a70e5aa2
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2023-01-05 12:16:22 +0000

    Using while() and do()

--- JavaScript/Class01/ex16.js
+++ JavaScript/Class01/ex16.js
@@ -1,13 +1,18 @@
 var count = 1
 
-while (count <= 6) {
+do { // Repeat 6 times while count is below 6
+    console.log(`${count} x`)
+    count++ //  count = count + 1 
+} while (count <= 6)
+
+
+
+/*
+while (count <= 6) {  // While count is below 6, repeat the console.log() command
     console.log(`${count} x`)
     count++ //  count = count + 1 
 }
-
-
-
-
+*/
 
 /* 
 OLD USE:
